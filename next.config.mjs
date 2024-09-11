d6bb5736e9dc8f0c13114dbd21f45c40b77caadb
vikas-011 <vikas.tech011@gmail.com>
2024-09-11 12:01:07 +0530

    new files

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/dfl8r2ylz/image/upload/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
+
 export default nextConfig;
